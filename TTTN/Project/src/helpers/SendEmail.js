85db58bd6934ba9264685914ab3af29868381a55
unknown <20111063883@hunre.edu.vn>
2024-02-26 14:43:08 +0700

    BE_CLASSOFFLINE

---
+++ TTTN/Project/src/helpers/SendEmail.js
@@ -0,0 +1,22 @@
+const nodemailer = require("nodemailer");
+
+module.exports = async (email, subject, html) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: process.env.MAIL_SECURE,
+    auth: {
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+  const info = await transporter.sendMail({
+    from: process.env.MAIL_USERNAME,
+    to: email,
+    subject: subject,
+    html: html,
+  });
+};
